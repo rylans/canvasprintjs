42c55545fde810deb1acbc0cf475203814642f58
Rylan <rylans@gmail.com>
2015-01-15 11:22:48 -0800

    Hash the data to 32 bits

--- canvasprint.js
+++ canvasprint.js
@@ -10,8 +10,19 @@ var Canvasprint = (function(Canvasprint) {
     get: function (){
       var print = 0;
       print = this.getPrint();
-      //TODO: Hash it
-      return print;
+      return this.hash(print);
+    },
+
+    hash: function(base64){
+      var hash = 3001, i, len, string = atob(base64);
+      if (string.length == 0) return hash;
+      var p = 0x7fffffff;
+      for (i = 0, len = string.length; i < len; i++){
+	hash = ((hash << 5) - hash) + (string.charCodeAt(i) * 47);
+	hash ^= p;
+	hash |= 0;
+      }
+      return hash;
     },
 
     getPrint: function() {
@@ -23,7 +34,11 @@ var Canvasprint = (function(Canvasprint) {
       context.fillStyle = "#fab";
       context.fillRect(40,13,20,30);
       context.fillText(txt,11,11);
-      return canv.toDataURL();
+      context.fillStyle = "rgba(101,202, 12, 0.66)";
+      context.fillText(txt,22,22);
+      context.font = "14px 'no-such-font-'";
+      context.fillText(txt,33,33);
+      return canv.toDataURL().replace("data:image/png;base64,","");
     }
   };
   return Canvasprint;
